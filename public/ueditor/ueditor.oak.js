a5350cc91503e1355980228adbc30a6930b154c3
刘伶俐 <1079238101@qq.com>
2017-07-26 13:26:53 +0800

    完善痕迹保留
    Signed-off-by: 刘伶俐 <1079238101@qq.com>

--- public/ueditor/ueditor.oak.js
+++ public/ueditor/ueditor.oak.js
@@ -18,7 +18,7 @@
  * 例子：
  * (new Date()).Format("yyyy-MM-dd HH:mm:ss.S") ==> 2006-07-02 08:09:04.423
  * (new Date()).Format("yyyy-M-d H:m:s.S")      ==> 2006-7-2 8:9:4.18
-*/
+ */
 Date.prototype.format = function (fmt) { //author: meizz
   var o = {
     "M+": this.getMonth() + 1, //月份
@@ -56,164 +56,174 @@ function ChangeWidget(){
   this.editor = window.top.UE.getEditor('ueditor'); //获取编辑器实例
   /* 获取编辑器内所有控件的原始值 */
   this.init = function (){
-     var that = this;
-     that.editor.ready(function(){
-       var allwidgets = this.execCommand('allwidgets');
-       that.allwidgets = allwidgets;
-       window.top.allwidgets = allwidgets;
-     })
+    var that = this;
+    that.editor.ready(function(){
+      var allwidgets = this.execCommand('allwidgets');
+      that.allwidgets = allwidgets;
+      window.top.allwidgets = allwidgets;
+    })
   }
   this.showChange = function(){
-     var that = this;
-     window.top.changes = that.changes;
-     that.editor.focus();
+    var that = this;
+    window.top.changes = that.changes;
+    that.editor.focus();
   }
 }
 ChangeWidget.prototype = {
-    change:function(){
-      var allwidgets = window.top.allwidgets||this.allwidgets;
-      //绑定事件
-      for(var i=0;i<allwidgets.length;i++){
-        var widget = allwidgets[i];
-        var type = widget.type;
-        var target = widget.target;
-        var value = widget.value;
-        switch (type){
-          case 'input' : this.inputChange(target,value,i);break;
-          case 'select' : this.selectChange(target,value,i);break;
-          case 'timeinput' : this.timeinputChange(target,value,i);break;
-          case 'checkbox' : this.checkboxChange(target,value,i);break;
-          case 'radio' : this.radioChange(target,value,i);break;
-          default:break;
-        }
+  change:function(){
+    var allwidgets = window.top.allwidgets||this.allwidgets;
+    //绑定事件
+    for(var i=0;i<allwidgets.length;i++){
+      var widget = allwidgets[i];
+      var type = widget.type;
+      var target = widget.target;
+      var value = widget.value;
+      switch (type){
+        case 'input' : this.inputChange(target,value,i);break;
+        case 'select' : this.selectChange(target,value,i);break;
+        case 'timeinput' : this.timeinputChange(target,value,i);break;
+        case 'checkbox' : this.checkboxChange(target,value,i);break;
+        case 'radio' : this.radioChange(target,value,i);break;
+        default:break;
       }
-    },
-    inputChange : function(target,value,key){ //input控件监听
-      var that = this;
-      var oldValue = value;
-      /* 用定时器监听，节流，优化效率 */
-      var timer = null;
-      function diff(oldValue,newValue){
-        var time = new Date().format("yyyy-MM-dd HH:mm:ss");
-        if(newValue != oldValue && newValue!=''){
+    }
+  },
+  inputChange : function(target,value,key){ //input控件监听
+    var that = this;
+    var oldValue = value;
+    /* 用定时器监听，节流，优化效率 */
+    var timer = null;
+    function diff(oldValue,newValue){
+      var time = new Date().format("yyyy-MM-dd HH:mm:ss");
+      if(newValue != oldValue && newValue!=''){
+        that.changes[key] = {
+          date : time,
+          author : '刘伶俐',
+          type : '修改了内容',
+          desc : oldValue+'修改成'+newValue
+        };
+      }else if(newValue != oldValue && newValue==''){
+        that.changes[key] = {
+          date : time,
+          author : '刘伶俐',
+          type : '删除了内容',
+          desc : '删除'+oldValue
+        };
+      }else{
+        delete that.changes[key];
+      }
+      that.showChange();
+    }
+    target.addEventListener('keyup',function(e){
+      var newValue = e.target.innerHTML;
+      clearTimeout(timer)
+      timer = setTimeout(function(){
+        diff(oldValue,newValue)
+      },400);
+    });
+
+  },
+  selectChange : function (target,value,key){ // slect下拉控件监听
+    var that = this;
+    var oldValue = value;
+    target.addEventListener('click',function(e){
+      var className = e.target.className||'';
+      if(className.indexOf('li') > -1){
+        newValue = e.target.innerText;
+        if(oldValue != newValue){ //改变
+          var time = new Date().format("yyyy-MM-dd HH:mm:ss");
           that.changes[key] = {
             date : time,
             author : '刘伶俐',
             type : '修改了内容',
             desc : oldValue+'修改成'+newValue
           };
-        }else if(newValue != oldValue && newValue==''){
-          that.changes[key] = {
-            date : time,
-            author : '刘伶俐',
-            type : '删除了内容',
-            desc : '删除'+oldValue
-          };
+        }else{
+          delete that.changes[key];
         }
         that.showChange();
       }
-      target.addEventListener('keyup',function(e){
-        var newValue = e.target.innerHTML;
-        clearTimeout(timer)
-        timer = setTimeout(function(){
-          diff(oldValue,newValue)
-        },400);
-      });
-
-    },
-    selectChange : function (target,value,key){ // slect下拉控件监听
-      var that = this;
-      var oldValue = value;
-      target.addEventListener('click',function(e){
-        var className = e.target.className||'';
-        if(className.indexOf('li') > -1){
-           newValue = e.target.innerText;
-           if(oldValue != newValue){ //改变
-             var time = new Date().format("yyyy-MM-dd HH:mm:ss");
-             that.changes[key] = {
-               date : time,
-               author : '刘伶俐',
-               type : '修改了内容',
-               desc : oldValue+'修改成'+newValue
-             };
-             that.showChange();
-           }
-        }
-      });
-    },
-    timeinputChange : function (target,value,key){
-      var that = this;
-      var oldValue = value;
-      var input = target.getElementsByTagName('input')[1];
-      var _my97DP = $dp.focusArr[0] ;
-      _my97DP.addEventListener('click',function(e){
-         var newValue = input.value;
-         if(newValue != oldValue){
-           var time = new Date().format("yyyy-MM-dd HH:mm:ss");
-           that.changes[key] = {
-             date : time,
-             author : '刘伶俐',
-             type : '修改了时间',
-             desc : oldValue+'修改成'+newValue
-           };
-           that.showChange();
-         }
-      })
-    },
-    checkboxChange : function (target,value,key){
-      var that = this;
-      var oldValue = value;
-      target.addEventListener('click',function(e){
-        if(e.target.tagName == 'INPUT'){
-           //查找所有input[type='checkbox'的元素]
-          var newValue = [];
-          var input = target.getElementsByTagName('input');
-          for(var i=0;i<input.length;i++){
-            if(input[i].checked){
-              newValue.push(input[i].value);
-            }
-          }
-          if(newValue != oldValue){
-            var time = new Date().format("yyyy-MM-dd HH:mm:ss");
-            that.changes[key] = {
-              date : time,
-              author : '刘伶俐',
-              type : '修改',
-              desc : oldValue+'修改成'+ newValue
-            };
-            that.showChange();
+    });
+  },
+  timeinputChange : function (target,value,key){
+    var that = this;
+    var oldValue = value;
+    var input = target.getElementsByTagName('input')[1];
+    var _my97DP = $dp.focusArr[0] ;
+    _my97DP.addEventListener('click',function(e){
+      var newValue = input.value;
+      if(newValue != oldValue){
+        var time = new Date().format("yyyy-MM-dd HH:mm:ss");
+        that.changes[key] = {
+          date : time,
+          author : '刘伶俐',
+          type : '修改了时间',
+          desc : oldValue+'修改成'+newValue
+        };
+      }else{
+        delete that.changes[key];
+      }
+      that.showChange();
+    })
+  },
+  checkboxChange : function (target,value,key){
+    var that = this;
+    var oldValue = value;
+    target.addEventListener('click',function(e){
+      if(e.target.tagName == 'INPUT'){
+        //查找所有input[type='checkbox'的元素]
+        var newValue = [];
+        var input = target.getElementsByTagName('input');
+        for(var i=0;i<input.length;i++){
+          if(input[i].checked){
+            newValue.push(input[i].value);
           }
         }
-        //this.showChange();
-      })
-    },
-    radioChange : function (target,value,key){
-      var that = this;
-      var oldValue = value;
-      target.addEventListener('click',function(e){
-        if(e.target.tagName == 'INPUT'){
-          //查找所有input[type='checkbox'的元素]
-          var newValue = [];
-          var input = target.getElementsByTagName('input');
-          for(var i=0;i<input.length;i++){
-            if(input[i].checked){
-              newValue.push(input[i].value);
-            }
-          }
-          if(newValue != oldValue){
-            var time = new Date().format("yyyy-MM-dd HH:mm:ss");
-            that.changes[key] = {
-              date : time,
-              author : '刘伶俐',
-              type : '修改',
-              desc : oldValue+'修改成'+ newValue
-            };
-            that.showChange();
+        if(newValue != oldValue){
+          var time = new Date().format("yyyy-MM-dd HH:mm:ss");
+          that.changes[key] = {
+            date : time,
+            author : '刘伶俐',
+            type : '修改',
+            desc : oldValue+'修改成'+ newValue
+          };
+        }else{
+          delete that.changes[key];
+        }
+        that.showChange();
+      }
+      //this.showChange();
+    })
+  },
+  radioChange : function (target,value,key){
+    var that = this;
+    var oldValue = value;
+    target.addEventListener('click',function(e){
+      if(e.target.tagName == 'INPUT'){
+        //查找所有input[type='checkbox'的元素]
+        var newValue = [];
+        var input = target.getElementsByTagName('input');
+        for(var i=0;i<input.length;i++){
+          if(input[i].checked){
+            newValue.push(input[i].value);
           }
         }
-        //this.showChange();
-      })
-    }
+        if(newValue != oldValue){
+          var time = new Date().format("yyyy-MM-dd HH:mm:ss");
+          that.changes[key] = {
+            date : time,
+            author : '刘伶俐',
+            type : '修改',
+            desc : oldValue+'修改成'+ newValue
+          };
+        }else{
+          delete that.changes[key];
+        }
+        that.showChange();
+      }
+      //this.showChange();
+    })
+  }
 };
 
 function ceshi(){
